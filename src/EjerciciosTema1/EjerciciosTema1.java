9cd1a75d58001142c0d878c3eb4ff9f4297a8ff6
pieroanguizvargas <pieroanguizvargas@outlook.com>
2023-02-10 11:45:38 -0500

    Initial commit

---
+++ src/EjerciciosTema1/EjerciciosTema1.java
@@ -0,0 +1,19 @@
+package EjerciciosTema1;
+
+public class EjerciciosTema1 {
+    public static void main(String[] args) {
+        int numero1 = 50;
+        byte numero2 = 5;
+        long numero3 = 100;
+        double numero4 = 100.50;
+        float numero5 = 100.99f;
+        short numero6 = 25;
+        boolean verdadero = true;
+        boolean falso = false;
+        char  palabra = 'a';
+        String  oracion = "El gato de Habsburgo";
+
+        System.out.println("" + numero1 + " " + numero2 + " " + numero3 + " " + numero4 + " "  +numero5 + " " + numero6 + " " + verdadero + " " + falso + " " + palabra + " " + oracion);
+
+    }
+}
